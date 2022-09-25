5681da58a1ff19f2bbee8c28d87434190da88489
Micheal onsongo <111071508+sensie200@users.noreply.github.com>
2022-09-25 10:03:12 +0300

    I added work on over fitting and under fitting of decision trees and intro the the random forest
    Overfitting and underfitting are about trying to find the sweet spot where the model makes the most accurate predictions. This is done by running the model at different heights of the decision tree and selecting the height at which the model has the least MSE.
    The random forest uses many trees, and it makes a prediction by averaging the predictions of each component tree. It generally has much better predictive accuracy than a single decision tree and it works well with default parameters. If you keep modeling, you can learn more models with even better performance, but many of those are sensitive to getting the right parameters.

--- price_predictor.py
+++ price_predictor.py
@@ -1,4 +1,5 @@
 from random import random
+from webbrowser import get
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 #reading the csv file
@@ -34,5 +35,38 @@ split_housing_model=DecisionTreeRegressor()
 split_housing_model.fit(train_x,train_y)
 split_housing_prediction=split_housing_model.predict(val_x)
 print(mean_absolute_error(val_y,split_housing_prediction))
+#over fitting and underfitting 
 
+def get_mae(max_leaf_nodes,train_x,val_x,train_y,val_y):
+    model=DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes,random_state=0)
+    model.fit(train_x,train_y)
+    preds_vals=model.predict(val_x)
+    mae=mean_absolute_error(val_y,preds_vals)
+    return mae
+
+'''for max_leaf_nodes in [5,50,500,5000]:
+    my_mae=get_mae(max_leaf_nodes,train_x,val_x,train_y,val_y)
+
+    print("Max leaf nodes :%d \t\t Mean absolute Error :%d"%(max_leaf_nodes,my_mae))'''
+candidate_max_leaf_nodes=[5,25,50,100,250,500]
+
+solution={leaf_size:get_mae(leaf_size,train_x,val_x,train_y,val_y) for leaf_size in candidate_max_leaf_nodes}
+best_tree_size=min(solution,key=solution.get)
+print(best_tree_size)
+print(solution)
+
+'''The random forest model
+The random forest uses many trees, and it makes a prediction by averaging the predictions of each component tree. 
+It generally has much better predictive accuracy than a single decision tree and it works well with default parameters. 
+If you keep modeling, you can learn more models with even better performance, 
+but many of those are sensitive to getting the right parameters.
+'''
+from sklearn.ensemble import RandomForestRegressor
+forest_model=RandomForestRegressor(random_state=1)
+forest_model.fit(train_x,train_y)
+forest_predicts=forest_model.predict(val_x)
+print("Here is the mean_absolute_erro for random_forest")
+'''See here the mean absolute error reduces then using the random 
+forest model '''
+print(mean_absolute_error(val_y,forest_predicts))
 
